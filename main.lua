c80bfbf11df5c570ecd7a994fd4376eff6a17160
brennop <brennopcordeiro@gmail.com>
2019-12-18 14:29:40 -0300

    Initial blur shader implementation Using this as base: https://github.com/Jam3/glsl-fast-gaussian-blur

--- main.lua
+++ main.lua
@@ -6,6 +6,29 @@ local Table = require 'objects/Table'
 
 -- global variable for debug displaying
 debug = {}
+shader_code = [[
+vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords){
+  vec4 pixel = Texel(texture, texture_coords);
+  float b = (pixel.r + pixel.b + pixel.g )/3.0;
+  vec4 outColor = pixel * b;
+  return vec4(outColor) ;
+}
+]]
+
+blur_code = [[
+extern vec2 screen;
+vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords){
+  vec4 iColor = vec4(0.0);
+  vec2 off1 = vec2(1.3846153846);
+  vec2 off2 = vec2(3.2307692308);
+  iColor += Texel(texture, texture_coords) * 0.29411764705882354;
+  iColor += Texel(texture, texture_coords + (off1 / screen)) * 0.3162162162;
+  iColor += Texel(texture, texture_coords - (off1 / screen)) * 0.3162162162;
+  iColor += Texel(texture, texture_coords + (off2 / screen)) * 0.0702702703;
+  iColor += Texel(texture, texture_coords - (off2 / screen)) * 0.0702702703;
+  return iColor * color;
+}
+]]
 
 function love.load()
   scene = Table()
@@ -24,6 +47,10 @@ function love.load()
   love.graphics.draw(love.graphics.newCanvas())
   love.graphics.setShader()
   love.graphics.setCanvas()
+
+  shader = love.graphics.newShader(shader_code)
+  blur = love.graphics.newShader(blur_code)
+  blur:send("screen", {love.graphics:getWidth(), love.graphics:getHeight()})
 end
 
 function love.update(dt)
@@ -31,6 +58,7 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.setShader(blur)
   love.graphics.draw(starfield)
 
   scene:draw()
@@ -38,4 +66,5 @@ function love.draw()
   for index,value in ipairs(debug) do
     love.graphics.print(value, 10, 10 + index*10)
   end
+  love.graphics.setShader()
 end
